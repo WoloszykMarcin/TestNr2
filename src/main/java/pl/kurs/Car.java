38b9e8aa40f21c68ce3b21d76d7883e3050fce9c
Marcin Woloszyk <marcinwoloszyk412@gmail.com>
2022-11-07 17:10:55 +0100

    added Car class

---
+++ src/main/java/pl/kurs/Car.java
@@ -0,0 +1,11 @@
+package pl.kurs;
+
+public class Car {
+    private String producer;
+    private String model;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+}
